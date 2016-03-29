f3b8284ba9f9f13532bd24a0ca54cbfe682d7370
cstefanache <cornel.stefanache@gmail.com>
2016-03-29 09:34:04 +0300

    Added min set of files for package

---
+++ ng2-components.ts
@@ -0,0 +1,9 @@
+///<reference path="node_modules/angular2/typings/browser.d.ts"/>
+
+import { NgGeneric } from './src/ngGeneric';
+
+export * from './src/ngGeneric';
+
+export default {
+    directives: [ NgGeneric ]
+}
---
+++ src/ngGeneric.ts
@@ -0,0 +1,8 @@
+import { Directive } from 'angular2/core';
+
+@Directive({
+    selector: '[ng-generic]'
+})
+export class NgGeneric {
+    constructor() {}
+}
